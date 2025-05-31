2c3e365beaf151b0a788507e28c48476614f5788
Szymon Skibiński <szymonskibinski21@gmail.com>
2025-05-31 22:55:20 +0200

    Dodano logowanie

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include "logowanie.h"
-
+#include "mainwindow.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Logowanie w;
-    w.show();
-    return a.exec();
+    Logowanie log;
+    if (log.exec() == QDialog::Accepted) {
+        MainWindow w;
+        w.show();
+        return a.exec();
+    }
+    return 0;
 }
